1dd0324577894d2f5d76492b439f120991d739be
Felipe Pessina <fs.pessina@gmail.com>
2024-02-20 19:11:22 +0400

    Parsing signature response

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,11 @@
 import React, { useEffect, useState } from "react";
 import initNear from "../config/near";
 import { Account } from "near-api-js";
-
-async function callContractFunction(account: Account) {
-  const result = await account.functionCall({
-    contractId: "multichain-testnet-2.testnet",
-    methodName: "sign",
-    args: {
-      payload: [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,
-        4, 5, 6, 7, 8, 9, 0, 2,
-      ],
-      path: "test",
-    },
-    gas: "100000000000000",
-    attachedDeposit: "0",
-  });
-
-  console.log("Function call result:", result);
-}
+import Loader from "@/components/Loader";
 
 export default function Home() {
   const [account, setAccount] = useState<Account | undefined>(undefined);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const init = async () => {
@@ -34,11 +18,42 @@ export default function Home() {
     init();
   }, []);
 
-  if (!account) return <div>Loading...</div>;
+  async function callContractFunction(account: Account) {
+    setIsLoading(true);
+    const result = await account.functionCall({
+      contractId: "multichain-testnet-2.testnet",
+      methodName: "sign",
+      args: {
+        payload: Array.from({ length: 32 }, () =>
+          Math.floor(Math.random() * 64)
+        ),
+        path: "test",
+      },
+      gas: "100000000000000",
+      attachedDeposit: "0",
+    });
+
+    if ("SuccessValue" in (result.status as any)) {
+      const successValue = (result.status as any).SuccessValue;
+      const decodedValue = Buffer.from(successValue, "base64").toString(
+        "utf-8"
+      );
+
+      console.log(JSON.parse(decodedValue));
+    }
+
+    setIsLoading(false);
+  }
 
   return (
-    <button onClick={() => callContractFunction(account)}>
-      Log Account Keys
-    </button>
+    <div className="h-screen w-full flex justify-center items-center">
+      {!account || isLoading ? (
+        <Loader />
+      ) : (
+        <button onClick={() => callContractFunction(account)}>
+          Send transaction
+        </button>
+      )}
+    </div>
   );
 }
