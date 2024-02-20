272f2fbd9f0b6b9e4e9fdc7188862e71bc6585ab
Felipe Pessina <fs.pessina@gmail.com>
2024-02-20 18:31:09 +0400

    Setup connection and keys

---
+++ config/near.ts
@@ -0,0 +1,25 @@
+import { KeyPair, connect, keyStores } from "near-api-js";
+
+if (!process.env.NEXT_PUBLIC_NEAR_PRIVATE_KEY) {
+  throw new Error("No private key found in environment");
+}
+
+const keyPair = KeyPair.fromString(process.env.NEXT_PUBLIC_NEAR_PRIVATE_KEY);
+const keyStore = new keyStores.InMemoryKeyStore();
+keyStore.setKey("testnet", "felipe-sandbox.testnet", keyPair);
+
+const config = {
+  networkId: "testnet",
+  keyStore,
+  nodeUrl: "https://rpc.testnet.near.org",
+  walletUrl: "https://wallet.testnet.near.org",
+  helperUrl: "https://helper.testnet.near.org",
+};
+
+async function initNear() {
+  const near = await connect(config);
+  const account = await near.account("felipe-sandbox.testnet");
+  return { near, account };
+}
+
+export default initNear;
