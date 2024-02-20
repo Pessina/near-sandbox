d3953d50d034f7e9188653fa27a1cc188c6c853c
Felipe Pessina <fs.pessina@gmail.com>
2024-02-20 18:52:55 +0400

    Function call working

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,44 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import initNear from "../config/near";
+import { Account } from "near-api-js";
 
-const logAccountKeys = async () => {
-  const { account } = await initNear();
-  try {
-    const accessKeys = await account.getAccessKeys();
-    console.log("Access Keys:", accessKeys);
-  } catch (error) {
-    console.error("Failed to fetch access keys:", error);
-  }
-};
+async function callContractFunction(account: Account) {
+  const result = await account.functionCall({
+    contractId: "multichain-testnet-2.testnet",
+    methodName: "sign",
+    args: {
+      payload: [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3,
+        4, 5, 6, 7, 8, 9, 0, 2,
+      ],
+      path: "test",
+    },
+    gas: "100000000000000",
+    attachedDeposit: "0",
+  });
+
+  console.log("Function call result:", result);
+}
 
 export default function Home() {
-  return <button onClick={logAccountKeys}>Log Account Keys</button>;
+  const [account, setAccount] = useState<Account | undefined>(undefined);
+
+  useEffect(() => {
+    const init = async () => {
+      const { account } = await initNear();
+      setAccount(account);
+    };
+
+    init();
+  }, []);
+
+  if (!account) return <div>Loading...</div>;
+
+  return (
+    <button onClick={() => callContractFunction(account)}>
+      Log Account Keys
+    </button>
+  );
 }
