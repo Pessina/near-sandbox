b4fb138c305825325cf8468c86001876fd96bf47
Felipe Pessina <fs.pessina@gmail.com>
2024-02-24 21:13:10 +0400

    Update Input styles

--- components/Input.tsx
+++ components/Input.tsx
@@ -9,7 +9,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(({ ...rest }, ref) => {
     <input
       {...rest}
       ref={ref}
-      className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+      className="block appearance-none w-full bg-gray-800 text-white border border-gray-600 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
     />
   );
 });
