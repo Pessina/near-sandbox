e803053ab42d7c5699d86b98add71361348972bb
Felipe Pessina <fs.pessina@gmail.com>
2024-02-26 13:26:07 +0400

    Add variants to buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import { AiOutlineLoading } from "react-icons/ai";
 
+type ButtonVariant = "primary" | "secondary" | "danger";
+
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   isLoading?: boolean;
+  variant?: ButtonVariant;
 }
 
+const variantStyles = {
+  primary: "bg-blue-500 hover:bg-blue-600 text-white",
+  secondary: "bg-gray-800 hover:bg-gray-700 text-white",
+  danger: "bg-red-500 hover:bg-red-600 text-white",
+};
+
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ children, className, isLoading, ...props }, ref) => {
+  ({ children, className, isLoading, variant = "primary", ...props }, ref) => {
     return (
       <button
         ref={ref}
-        className={`flex items-center justify-center px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-50 ${className}`}
+        className={`flex items-center justify-center px-4 py-2 border border-gray-600 hover:border-gray-500 rounded shadow leading-tight focus:outline-none focus:shadow-outline ${variantStyles[variant]} ${className}`}
         {...props}
         disabled={isLoading}
       >
