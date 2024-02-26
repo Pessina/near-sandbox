08570973eb905e6549fa9b569612d47b3e203e6d
Felipe Pessina <fs.pessina@gmail.com>
2024-02-26 14:26:51 +0400

    Fix icon

--- components/Select.tsx
+++ components/Select.tsx
@@ -11,30 +11,32 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
 const Select = forwardRef<HTMLSelectElement, SelectProps>(
   ({ options, placeholder, label, ...rest }, ref) => {
     return (
-      <div className="inline-block relative w-64">
+      <div>
         {label && <Label>{label}</Label>}
-        <select
-          ref={ref}
-          {...rest}
-          className="block appearance-none w-full bg-gray-800 text-white border border-gray-600 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-        >
-          {placeholder && (
-            <option value="" disabled style={{ color: "gray" }}>
-              {placeholder}
-            </option>
-          )}
-          {options.map((option) => (
-            <option
-              key={option.value}
-              value={option.value}
-              style={{ color: "black" }}
-            >
-              {option.label}
-            </option>
-          ))}
-        </select>
-        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-300">
-          <AiOutlineDown className="h-4 w-4" />
+        <div className="inline-block relative w-64">
+          <select
+            ref={ref}
+            {...rest}
+            className="block appearance-none w-full bg-gray-800 text-white border border-gray-600 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+          >
+            {placeholder && (
+              <option value="" disabled style={{ color: "gray" }}>
+                {placeholder}
+              </option>
+            )}
+            {options.map((option) => (
+              <option
+                key={option.value}
+                value={option.value}
+                style={{ color: "black" }}
+              >
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2 text-gray-300">
+            <AiOutlineDown className="h-4 w-4" />
+          </div>
         </div>
       </div>
     );
