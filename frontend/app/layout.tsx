e234efdd09766b6ea136d9a1072084e677b88f3a
Pessina <fs.pessina@gmail.com>
2024-12-14 22:25:44 +0700

    Clean up

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -6,7 +6,6 @@ import { KeyPairAuthProvider } from "@/providers/KeyPairAuthProvider";
 import Header from "@/components/Header";
 import "./globals.css";
 import { WagmiProvider } from "@/providers/WagmiProvider";
-import { ThemeProvider } from "next-themes"
 import { ReactQueryProvider } from "@/providers/ReactQueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -24,28 +23,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
+    <html lang="en" className="h-full dark">
       <body className={`${inter.className} h-full`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          disableTransitionOnChange>
-          <WagmiProvider>
-            <ReactQueryProvider>
-              <KeyPairAuthProvider>
-                <WalletAuthProvider>
-                  <main className="container mx-auto p-4 space-y-6 h-full flex flex-col">
-                    <Header />
-                    <div className="grow flex flex-col">
-                      {children}
-                    </div>
-                  </main>
-                  <Toaster />
-                </WalletAuthProvider>
-              </KeyPairAuthProvider>
-            </ReactQueryProvider>
-          </WagmiProvider>
-        </ThemeProvider>
+        <WagmiProvider>
+          <ReactQueryProvider>
+            <KeyPairAuthProvider>
+              <WalletAuthProvider>
+                <main className="container mx-auto p-4 space-y-6 h-full flex flex-col">
+                  <Header />
+                  <div className="grow flex flex-col">
+                    {children}
+                  </div>
+                </main>
+                <Toaster />
+              </WalletAuthProvider>
+            </KeyPairAuthProvider>
+          </ReactQueryProvider>
+        </WagmiProvider>
       </body>
     </html>
   );
